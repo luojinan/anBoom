b8a76f50da3023ee7fb669ab471443675e5f47d7
KingAn <1017386624@qq.com>
2022-10-17 11:49:54 +0800

    高阶函数学习-ts实现some、every

--- packages/playground/higher_order_function/index.ts
+++ packages/playground/higher_order_function/index.ts
@@ -1,7 +1,21 @@
-import { map } from "./map_some_every"
+import { every, map, some } from "./map_some_every"
 
 const list = [1,2,3,4]
-map(list,(item, index, arr)=>{
+const mapRes = map(list, (item, index, arr)=>{
   // console.log(item.length)
-  console.log(item,index,arr)
+  // console.log(item,index,arr)
+  return item+1
 })
+console.log('mapRes',mapRes)
+
+const someRes = some(list, (item)=>{
+  // console.log(item.length)
+  return item > 3
+})
+console.log('someRes',someRes)
+
+const everyRes = every(list, (item)=>{
+  // console.log(item.length)
+  return item < 4
+})
+console.log('everyRes',everyRes)
--- packages/playground/higher_order_function/map_some_every/index.ts
+++ packages/playground/higher_order_function/map_some_every/index.ts
@@ -1,14 +1,50 @@
 /**
- * map
+ * map() 方法创建一个新数组，这个新数组由原数组中的每个元素都调用一次提供的函数后的返回值组成。
  * @param list 
- * @param fn 
+ * @param callback 
  * @returns 
  */
-export function map<T,U>(list: T[], fn: (item: T, index: number, list: readonly T[])=> U): U[] {
+export function map<T,U>(list: T[], callback: (item: T, index: number, list: readonly T[])=> U): U[] {
   const resultList = []
 
-  for(let i = 0; i <= list.length ; i++){
-    resultList.push(fn(list[i], i, list))
+  for(let i = 0; i < list.length ; i++){
+    resultList.push(callback(list[i], i, list))
   }
   return resultList
+}
+
+/**
+ * some() 方法测试数组中是不是至少有 1 个元素通过了被提供的函数测试。它返回的是一个 Boolean 类型的值。
+ * @param list 
+ * @param callback 
+ * @returns 
+ */
+export function some<T>(list: T[], callback: (item:T, index: number, list: T[])=>boolean): boolean {
+  let result = false
+  
+  for(let i = 0; i < list.length ; i++){
+    result = callback(list[i], i, list)
+    if(result) {
+      break
+    }
+  }
+  return result
+}
+
+/**
+ * every() 方法测试一个数组内的所有元素是否都能通过某个指定函数的测试。它返回一个布尔值。
+ * @param list 
+ * @param callback 
+ * @returns 
+ */
+export function every<T>(list: T[], callback: (item:T, index: number, list: T[])=>boolean): boolean {
+  let result = true
+  
+  for(let i = 0; i < list.length ; i++){
+    result = callback(list[i], i, list)
+    if(!result) {
+      break
+    }
+  }
+  return result
 }
